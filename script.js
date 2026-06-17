1c7993f827971d9062e552112ef4fcd4b8d9ec9a
Siddharth Deshetti <58861396+sidsd11@users.noreply.github.com>
2026-06-17 20:35:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const makeChange = (c) => {
   // your name here
+	let q = Math.floor(c / 25)
+	let d = Math.floor((c - (q * 25)) / 10)
+	let n = Math.floor((c - (q * 25) - (d * 10)) / 5)
+	let p = c - (q * 25) - (d * 10) - (n * 5)
+	return {'q': q, 'd': d, 'n': n, 'p': p}
 };
-
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
